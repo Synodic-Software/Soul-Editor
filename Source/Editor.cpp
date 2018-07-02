bd77b1edaa372b7d8b526b1ce65b5e36a2cb1873
Behemyth <asher.norland@gmail.com>
2018-07-02 02:37:07 -0500

    Require GLM + Create Window

--- Source/Editor.cpp
+++ Source/Editor.cpp
@@ -2,13 +2,31 @@
 
 Editor::Editor(SoulParameters params) :
 	SoulApplication(params) {
-	
+
 }
 
 int main(int, char*[])
 {
+	//app params
+	SoulParameters appParams;
+	Editor app(appParams);
 
-	Editor app;
+	//create the window
+	WindowParameters windowParams;
+	windowParams.type = WindowType::WINDOWED;
+	windowParams.title = "Main";
+	windowParams.monitor = 0;
+	windowParams.pixelPosX = 0;
+	windowParams.pixelPosY = 0;
+	windowParams.pixelWidth = 512;
+	windowParams.pixelHeight = 512;
+
+	Window* window = app.CreateWindow(windowParams);
+
+	//Window* mainWindow = CreateWindow(singleWindowType, "main", monitor, xPos, yPos, xSize, ySize);
+
+	//SetWindowLayout(mainWindow, new SingleLayout(new RenderWidget()));
+
 
 	app.Run();
 
